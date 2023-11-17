6e8c10fd11fa65438acd78975340403644c8a30a
Ivanio1 <sobolev1van@yandex.ru>
2023-11-17 16:49:12 +0300

    Start login/registration

---
+++ car-service-crm/src/main/java/carservicecrm/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package carservicecrm.repository;
+
+import carservicecrm.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    User findByEmail(String email);
+}
