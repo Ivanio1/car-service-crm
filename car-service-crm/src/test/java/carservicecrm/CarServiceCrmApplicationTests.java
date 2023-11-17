721b33e36b371a467f0713c8f3266bf9fe27bcf4
Ivanio1 <sobolev1van@yandex.ru>
2023-11-17 14:21:14 +0300

    Initial commit

---
+++ car-service-crm/src/test/java/carservicecrm/CarServiceCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package carservicecrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarServiceCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
